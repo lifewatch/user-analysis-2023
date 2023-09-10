6f3dac9dcb121d3d8f726008006155f57fcb945f
Marc Portier <marc.portier@gmail.com>
2023-09-10 16:25:59 +0200

    getting the graphdb to work together with the sparqlwrapper
    this marks an important milestone for #4 as we can now insert triples!

--- docker/lwua-ingest/lwua-py/lwua/ingest.py
+++ docker/lwua-ingest/lwua-py/lwua/ingest.py
@@ -9,22 +9,57 @@ def run_ingest():
     log.info("run_ingest")
 
 
-def temp_code_testing():
-    hostname = "localhost"
-    port = 7200
+def gdb_from_config(insert: bool = False):
+    # todo get base and repoid from .env
+    base = "http://localhost:7200"
     repoid = "lwua23"
 
-    sparql = SPARQLWrapper(f"http://{ hostname }:{ port }/repositories/{ repoid }")
+    endpoint = f"{ base }/repositories/{ repoid }"
+    updateEndpoint = endpoint + "/statements"   # update statements are handled at other endpoint
 
-    file = "data/project.ttl"
-    # convert the file to n3
+    gdb = SPARQLWrapper(
+        endpoint=endpoint,
+        updateEndpoint=updateEndpoint,
+        returnFormat='json',
+        agent="lwua-python-sparql-client"
+    )
+    gdb.method = 'POST'
+    return gdb
+
+
+def ingest_testing():
+
+    file = "../../data/project.ttl"
+    # todo convert the file to n3
+    # for now hardcoded sample:
     n3str = """
-    <http://example.com/lwua23> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://schema.org/Project> .
+    <http://example.com/lwua23-from-py> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://schema.org/Project> .
     """
+    
+    # define a graph -- todo find better id
     graphid = f"https://example.org/lwua23/{ file }"
-    inserts = f"INSERT DATA {{ GRAPH <{ graphid }> { n3str } }}"
+    # assemble the insert statement
+    inserts = f"INSERT DATA {{ GRAPH <{ graphid }> {{ { n3str } }} }}"
+    print(inserts)
 
-    sparql.setQuery(inserts)
-    sparql.setReturnFormat(JSON)
-    results = sparql.query().convert()
+    gdb = gdb_from_config()
+    gdb.setQuery(inserts)
+    gdb.queryType = 'INSERT'  # important to indicate that the update Endpoint should be used
+
+    results = gdb.query().convert()
+    print(results)
     return results
+
+
+def main():
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+    reqlog = logging.getLogger('requests.packages.urllib3')
+    reqlog.setLevel(logging.DEBUG)
+    reqlog.propagate = True
+
+    ingest_testing()
+
+
+if __name__ == '__main__':
+    main()
