4731fe7e16c8a0a7d15b51f28f22e63875222e18
cedricd <cedricdecruw@gmail.com>
2023-11-17 01:03:12 +0100

    small refactoring
    - deleted watchdog => not needed
    - deleted project2.ttl => not useful

--- docker/lwua-ingest/lwua-py/lwua/graph_functions.py
+++ docker/lwua-ingest/lwua-py/lwua/graph_functions.py
@@ -36,24 +36,24 @@ def gdb_from_config():
     gdb.method = 'POST'
     return gdb
 
-def ingest_graph(graph: Graph, context: str = None, replace_context: str = None):
+def ingest_graph(graph: Graph, context: str = None, replace: bool = False):
     log.debug(f"to insert data into <{ context }>")
 
     gdb = gdb_from_config()
 
     # do the cleanup if possible
-    if replace_context is not None and context is not None:
-        log.debug(f"deleting <{ replace_context }> before insert")
-        delete_graph(replace_context, gdb)
+    if replace and context is not None:
+        log.debug(f"deleting <{ context }> before insert")
+        delete_graph(context, gdb)
         
     # extract the triples and format the insert statement
     ntstr = graph.serialize(format="nt")
-    log.debug(f"extracted tiples == { ntstr }")
+    #log.debug(f"extracted tiples == { ntstr }") # comented out because it is too verbose
     if context is not None:
         inserts = f"INSERT DATA {{ GRAPH <{ context }> {{ { ntstr } }} }}"
     else:
         inserts = f"INSERT DATA {{ { ntstr} }}"
-    log.debug(f"INSERT of ==> { inserts }")
+    #log.debug(f"INSERT of ==> { inserts }")
 
     gdb.setQuery(inserts)
     log.debug(f"detected querytype == {gdb.queryType}")
@@ -61,46 +61,71 @@ def ingest_graph(graph: Graph, context: str = None, replace_context: str = None)
     # unsure if this is needed -- can sqlwrapper detect this automatically?
     gdb.queryType = 'INSERT'  # important to indicate that the update Endpoint should be used
     gdb.query()
-    
-    # add the graph to the admin graph
-    add_graph_to_admin(context,gdb)
+    register_context_lastmod(context,gdb)
 
 def named_context(name: str, base: str = URN_BASE):
     return f"{base}:{name}"   # TODO maybe consider something else?
 
 def fname_2_context(fname: str):
-    return named_context(f"data/{fname}")
+    #return named_context(f"data/{fname}") # /data prefix is not needed until we have multiple data folders to ingest from
+    return named_context(fname)
 
 def admin_context():
     return named_context("ADMIN")
 
-def add_graph_to_admin(context: str, gdb: SPARQLWrapper = None):
+def register_context_lastmod(context: str, gdb: SPARQLWrapper = None):
     if URN_BASE not in context:
         context = named_context(context)
     if gdb is None:
         gdb = gdb_from_config()
-    inserts = f"INSERT DATA {{ GRAPH <{ admin_context() }> {{ <{ context }> <urn:lwua:INGEST:LASTMOD> \"{time.time()}\"^^<http://www.w3.org/2001/XMLSchema#double> }} }}"
+        
+    c_time = time.time()
+    
+    #check if the context is already in the admin graph, if so check if the last modified time of the return is smaller than the current time
+    #if so, update the last modified time of the context
+    #if not, do nothing
+    lastmod = get_context_lastmod(context,gdb)
+    if lastmod['results']['bindings']:
+        if float(lastmod['results']['bindings'][0]['m']['value']) < c_time:
+            #delete the old last modified time
+            delete_context_from_admin(context,gdb)
+    
+    #tsparql for insert
+    # GRAPH <{ admin_context() }> {{ <{ context }> <https://schema.org/dateModified> \"{time.time()}\"^^<http://www.w3.org/2001/XMLSchema#double> }}
+    inserts = f"INSERT DATA {{ GRAPH <{ admin_context() }> {{ <{ context }> <https://schema.org/dateModified> \"{time.time()}\"^^<http://www.w3.org/2001/XMLSchema#double> }} }}"
     gdb.setQuery(inserts)
     gdb.queryType = 'INSERT'
     gdb.query()
+
+def get_context_lastmod(context: str, gdb: SPARQLWrapper = None):
+    if URN_BASE not in context:
+        context = named_context(context)
+    if gdb is None:
+        gdb = gdb_from_config()
+    #get last modified time of context
+    query = f"SELECT ?m WHERE {{ GRAPH <{ admin_context() }> {{ <{ context }> <https://schema.org/dateModified> ?m }} }}"
+    gdb.setQuery(query)
+    gdb.setReturnFormat(JSON)
+    results = gdb.query().convert()
+    return results
 
 def get_admin_graph(gdb: SPARQLWrapper = None):
     if gdb is None:
         gdb = gdb_from_config()
     #get full admin graph
-    query = f"SELECT ?g ?m WHERE {{ GRAPH <{ admin_context() }> {{ ?g <urn:lwua:INGEST:LASTMOD> ?m }} }}"
+    query = f"SELECT ?g ?m WHERE {{ GRAPH <{ admin_context() }> {{ ?g <https://schema.org/dateModified> ?m }} }}"
     gdb.setQuery(query)
     gdb.setReturnFormat(JSON)
     results = gdb.query().convert()
     return results
 
 
-def delete_graph_from_admin(context: str, gdb: SPARQLWrapper = None):
+def delete_context_from_admin(context: str, gdb: SPARQLWrapper = None):
     if URN_BASE not in context:
         context = named_context(context)
     if gdb is None:
         gdb = gdb_from_config()
-    deletes = f"DELETE WHERE {{ GRAPH <{ admin_context() }> {{ <{ context }> <urn:lwua:INGEST:LASTMOD> ?m }} }}"
+    deletes = f"DELETE WHERE {{ GRAPH <{ admin_context() }> {{ <{ context }> <https://schema.org/dateModified> ?m }} }}"
     gdb.setQuery(deletes)
     gdb.queryType = 'DELETE'
     gdb.query()
@@ -122,7 +147,7 @@ def delete_graph(context: str,gdb: SPARQLWrapper= None):
     gdb.query()
     
     #delete the graph from the admin graph
-    delete_graph_from_admin(context,gdb)
+    delete_context_from_admin(context,gdb)
 
 def suffix_2_format(suffix):
     if suffix in ["ttl", "turtle"]:
@@ -137,15 +162,21 @@ def read_graph(fpath: Path, format: str = None):
     graph: Graph = Graph().parse(location=str(fpath), format=format)
     return graph
 
-def ingest_data_file(fname, context: str = None, replace_context: str = None):
+def delete_data_file(fname):
+    context = fname_2_context(fname)
+    log.info(f"deleting {fname} from {context}")
+    delete_graph(context)
+
+def ingest_data_file(fname, context: str = None, replace: bool = False):
     file_path = data_path_from_config() / fname
     assert file_path.exists(), f"cannot ingest file at {file_path}"
 
     graph = read_graph(file_path)
-    iri_context = named_context(context)
-    iri_replace_context = named_context(replace_context) if replace_context is not None else None
     
-    log.info(f"ingesting {file_path} into {iri_context} replacing {iri_replace_context}")
+    if context is None:
+        context = fname_2_context(fname)
+    
+    log.info(f"ingesting {file_path} into {context} | replace : {replace}")
 
-    ingest_graph(graph, context=iri_context, replace_context=iri_replace_context)
+    ingest_graph(graph, context=context, replace=replace)
     # TODO maintain metadata triples last-ingest / last-modified of ingested file in some admin graph context
--- docker/lwua-ingest/lwua-py/lwua/tryout_watch.py
+++ docker/lwua-ingest/lwua-py/lwua/tryout_watch.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 import logging
 from lwua.helpers import enable_logging, resolve_path
-from lwua.graph_functions import delete_graph, ingest_data_file, get_admin_graph
+from lwua.graph_functions import ingest_data_file, delete_data_file, get_admin_graph
 
 log = logging.getLogger(__name__)
 
@@ -13,11 +13,10 @@ URN_BASE = "urn:lwua:INGEST" # !TODO: shouldn't this be in the env file?
 ### Cedric Decruw - 2023-11-16 ###
 # ! watchdog was not used due to the fact that the docker container was not able to access the host filesystem
 # ! this is a simple file watcher that will be used to trigger the ingest process
-class FileStore:
+class Observer:
     def __init__(self, directory):
         self.directory = directory
         self.files = self.get_all_files()
-        self.last_files = None
 
     def get_all_files(self):
         return {f: [os.path.getmtime(f)] for f in Path(self.directory).glob('**/*') if f.is_file()}
@@ -28,127 +27,76 @@ class FileStore:
         deleted = {f: self.files[f] for f in self.files if f not in new_files}
         modified = {}
         for f in new_files:
-            if f in self.files and new_files[f][0] != self.files[f][-1]:
-                if len(self.files[f]) == 2:
-                    self.files[f].pop(0)
-                self.files[f].append(new_files[f][0])
+            if f in self.files and new_files[f] != self.files[f]:
+                self.files[f] = new_files[f]
                 modified[f] = self.files[f]
-        self.last_files = self.files
+      
         self.files = new_files
-        return added, deleted, modified, self.last_files
+        return added, deleted, modified
 
 class Watcher:
     def __init__(self, directory_to_watch):
         self.directory_to_watch = resolve_path(os.getenv("GDB_DATA_FOLDER", os.path.join("/root/graphdb-import",directory_to_watch)), "dotenv").absolute()
         log.info(f"env pointing to { self.directory_to_watch }")
-        self.file_store = FileStore(self.directory_to_watch)
+        self.observer = Observer(self.directory_to_watch)
+        self.first_loop = True
 
     def run(self):
         try:
             while True:
-                if self.file_store.last_files is None:
+                if self.first_loop:
                     log.info("First time loop")
-                    
                     #try and get the graph , if this fails due to the graphdb server not being up yet, the watcher will try again in 5 seconds untill it succeeds
                     get_admin = True
                     while get_admin:
                         try:
+                            #get admin graph
                             admin_graph = get_admin_graph()
-                            log.info(f"admin graph: {admin_graph}")
-                            # admin graph looks like {'head': {'vars': ['g', 'm']}, 'results': {'bindings': [{'g': {'type': 'uri', 'value': 'urn:lwua:INGEST#admin-lwua-ingest'}, 'm': {'datatype': 'http://www.w3.org/2001/XMLSchema#dateTime', 'type': 'literal', 'value': '2021-09-16T14:00:00Z'}}}]}}
-                            # we need to extract all the g.value's and put them in a list 
-                            all_graphs_unformatted = [g['g']['value'] for g in admin_graph['results']['bindings']]
-                            #replace the URN_BASE with the empty string to get the context
-                            all_graphs = [g.replace(f"{URN_BASE}:","") for g in all_graphs_unformatted]
-                            
-                            #from this extract the name and last modified time of the graph by splitting on the last "_"
-                            info_admin_graph = []
-                            for g in all_graphs:
-                                info_g = {}
-                                #split the string on the last "_" to get the name and last modified time
-                                name, last_modified = g.rsplit("_",1)
-                                info_g['name'] = name
-                                info_g['last_modified'] = last_modified
-                                info_admin_graph.append(info_g)
-                            
-                            log.info(f"all graphs: {info_admin_graph}")
-                            get_admin = False
-                            
-                            #get all the files in the data folder recursively
-                            all_files = self.file_store.get_all_files()
-                            #compare the files to the admin graph 
+                            info_admin = [(g['g']['value'].replace(f"{URN_BASE}:", ''), g['m']['value']) for g in admin_graph['results']['bindings']]
+                            all_files = self.observer.get_all_files()
+                            #compare the files to the admin graph
                             # if filename is not in the admin graph, add it
                             # if filename is in the admin graph, check if the last modified time is the same, if not update the graph
                             # if filename is in the admin graph, but not in the files, delete the graph
-                            
+                            info_admin_dict = {g[0]: g[1] for g in info_admin}
                             for f in all_files:
-                                #check if the file is in the admin graph
-                                if f not in all_graphs:
-                                    #add the file to the graph
-                                    context = f"{f}_{all_files[f][0]}"
-                                    log.info(f"context to be used to add: {context}")
-                                    ingest_data_file(f,context)
-                                else:
-                                    #check if the last modified time is the same
-                                    if all_files[f][0] != info_admin_graph[all_graphs.index(f)]['last_modified']:
-                                        #update the graph
-                                        context_to_delete = f"{f}_{info_admin_graph[all_graphs.index(f)]['last_modified']}"
-                                        context_to_add = f"{f}_{all_files[f][0]}"
-                                        log.info(f"context to be used to delete: {context_to_delete}")
-                                        log.info(f"context to be used to add: {context_to_add}")
-                                        ingest_data_file(f,context_to_add, context_to_delete)
+                                if f in info_admin_dict:
+                                    # !TODO: check variables since the modified now just deletes the graph :/
+                                    if info_admin_dict[f] < all_files[f][0]:
+                                        log.info(f"File {f} has been modified since downtime, updating graph")
+                                        ingest_data_file(f, None, True)
+                                    continue
+                                log.info(f"File {f} has been added since downtime, adding graph")
+                                ingest_data_file(f)
+
+                            for g in info_admin:
+                                if g[0] not in all_files:
+                                    log.info(f"File {g[0]} has been deleted since downtime, deleting graph")
+                                    delete_data_file(g[0])
                             
-                            #check if there are any graphs in the admin graph that are not in the files
-                            for g in info_admin_graph:
-                                if g['name'] not in all_files:
-                                    log.info(f"graph {g['name']} not in files, deleting")
-                                    #delete the graph
-                                    context = f"{g['name']}_{g['last_modified']}"
-                                    log.info(f"context to be used to delete: {context}")
-                                    delete_graph(context)
-                            
-                        except:
-                            log.info("graphdb server not up yet, trying again in 1 second")
+                        except Exception as e:
+                            log.error(f"error: {e}")
                             time.sleep(1)
                             continue
+                        
+                        get_admin = False
+                    self.first_loop = False
 
                 log.info("Checking for updates")
-                added, deleted, modified, previous_filestore = self.file_store.update()
+                added, deleted, modified = self.observer.update()
                 for f in added:
-                    log.info(f"File {f} has been added at {time.ctime(added[f][0])}")
-                    #context = f"{f}_{format_time(added[f][0])}"
-                    context = f"{f}_{added[f][0]}"
-                    log.info(f"context to be used to add: {context}")
-                    ingest_data_file(f,context)
+                    log.info(f"File {f} has been added ")
+                    ingest_data_file(f)
                 for f in deleted:
                     log.info(f"File {f} has been deleted")
-                    #find the file in the previous filestore and use the last modified time to delete the graph
-                    #context = f"{f}_{format_time(previous_filestore[f][-1])}"
-                    context = f"{f}_{previous_filestore[f][-1]}"
-                    log.info(f"context to be used to delete: {context}")
-                    delete_graph(context)
+                    delete_data_file(f)
                 for f in modified:
-                    log.info(f"File {f} has been modified at {time.ctime(modified[f][-1])}, previous to last modified time: {time.ctime(modified[f][0]) if len(modified[f]) > 1 else 'N/A'}")
-                    #context_to_delete = f"{f}_{format_time(modified[f][0])}"
-                    #context_to_add = f"{f}_{format_time(modified[f][-1])}"
-                    context_to_delete = f"{f}_{modified[f][0]}"
-                    context_to_add = f"{f}_{modified[f][-1]}"
-                    log.info(f"context to be used to delete: {context_to_delete}")
-                    log.info(f"context to be used to add: {context_to_add}")
-                    ingest_data_file(f,context_to_add, context_to_delete)
+                    log.info(f"File {f} has been modified")
+                    ingest_data_file(f,None, True)
                     
                 time.sleep(5)
         except KeyboardInterrupt:
             log.info("Stopping watcher")     
-
-#left out this part of the code since it is easier to just use the epoch time as the context , that way there is no need to reverse the process for the startup of the watcher
-'''      
-def format_time(epoch_time):
-    # Convert epoch time to struct_time object
-    time_obj = time.localtime(epoch_time)
-    # Format to return %Y_%m_%d_%H_%M_%S
-    return time.strftime("%Y_%m_%d_%H_%M_%S", time_obj)
-'''     
 
 ## test the watcher on local file system - not in docker
 if __name__ == '__main__':
