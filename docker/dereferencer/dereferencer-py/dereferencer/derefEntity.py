29417c6a097d8e19e1ccb1f79a718efd1983f2e7
cedricd <cedricdecruw@gmail.com>
2023-12-01 18:06:12 +0100

    deleted metadata management for now in search for more favorable system

--- docker/dereferencer/dereferencer-py/dereferencer/derefEntity.py
+++ docker/dereferencer/dereferencer-py/dereferencer/derefEntity.py
@@ -12,61 +12,15 @@ from .helpers import resolve_path
 log = logging.getLogger(__name__)
 
 
-def metadata_folder_from_config():
-    local_default = str(resolve_path("./memory_dereferencer", versus="dotenv"))
-    folder_name = os.getenv("METADATA_FILES_FOLDER", local_default)
-    return folder_name
-
-
 def data_folder_from_config():
     local_default = str(resolve_path("../data", versus="dotenv"))
     folder_name = os.getenv("DATA_FOLDER", local_default)
     return folder_name
 
 
-def check_metadata_folder_exists():
-    metadata_folder = metadata_folder_from_config()
-    if not os.path.exists(metadata_folder):
-        log.info(
-            f"metadata folder {metadata_folder} does not exist, creating it")
-        os.makedirs(metadata_folder)
 
 
 DATA_FOLDER = data_folder_from_config()
-METADATA_FOLDER = metadata_folder_from_config()
-METADATA_FILE = METADATA_FOLDER + "/metadata.json"
-
-
-def make_metadata(input_list):
-    result = []
-
-    if input_list is None:
-        return result
-
-    for item in input_list:
-        if isinstance(item, dict):
-            for key, values in item.items():
-                if isinstance(values, list):
-                    children = make_metadata(values)
-                    result.append(
-                        {key: {"completed": False, "children": children}})
-                else:
-                    result.append({values: {"completed": False}})
-        elif isinstance(item, str):
-            result.append({item: {"completed": False}})
-
-    return result
-
-
-def save_metadata(uri: str, data: object, completed: bool = False):
-    """saves the metadata to file"""
-    with open(METADATA_FILE, "r") as f:
-        # read the json file
-        current_metadata = json.load(f)
-    # replace the metadata for the uri with the new metadata
-    current_metadata[uri] = {"completed": completed, "metadata": data}
-    with open(METADATA_FILE, "w") as f:
-        json.dump(current_metadata, f)
 
 
 class DerefUriEntity:
@@ -75,38 +29,9 @@ class DerefUriEntity:
         self.propertypathmetadata = None
         self.propertypaths = propertypaths
 
-        check_metadata_folder_exists()
-        if not os.path.exists(METADATA_FILE):
-            log.info(
-                f"metadata file {METADATA_FILE} does not exist, creating it")
-            with open(METADATA_FILE, "w") as f:
-                json.dump({}, f)
-        else:
-            with open(METADATA_FILE, "r") as f:
-                self.metadata = json.load(f)
-            self.propertypathmetadata = self.get_metadata_uri(uri)
-
-        log.debug(f"propertypathmetadata: {self.propertypathmetadata}")
-
-        # if propertyMetadata is None then run function make metadata
-        if self.propertypathmetadata is None:
-            self.propertypathmetadata = make_metadata(self.propertypaths)
-            log.info(f"propertypathmetadata: {self.propertypathmetadata}")
-            save_metadata(self.uri, self.propertypathmetadata)
         self.download_uri(uri)
-        self.run_download_propertypaths()
-
-    def get_metadata_uri(self, uri: str):
-        """checks if a given uri is in the metadata
-
-        :param uri: uri to check
-        :type uri: str
-        :return: dict with the metadata for the uri
-        """
-        if uri in self.metadata:
-            return self.metadata[uri]["metadata"]
-        else:
-            return None
+        if propertypaths is not None:
+            self.run_download_propertypaths()
 
     def download_uri(self, uri: str):
         """downloads the uri either in json-ld or ttl format and puts the result in the DATA_FOLDER
@@ -145,5 +70,5 @@ class DerefUriEntity:
     # This function cannot be run atm , first download main self.uri
     def run_download_propertypaths(self):
         """runs the download_propertypaths function for all propertypaths"""
-        for propertypath in self.propertypathmetadata:
+        for propertypath in self.propertypaths:
             log.info(f"running download_propertypath for {propertypath}")
--- docker/dereferencer/dereferencer-py/dereferencer/graph.py
+++ docker/dereferencer/dereferencer-py/dereferencer/graph.py
@@ -3,6 +3,7 @@
 
 import logging
 from SPARQLWrapper import SPARQLWrapper, JSON
+from urllib.parse import unquote, quote
 import logging
 import os
 import re
@@ -10,6 +11,7 @@ import re
 # from dotenv import load_dotenv
 
 log = logging.getLogger(__name__)
+URN_BASE = os.getenv("URN_BASE", "urn:lwua:INGEST")
 
 
 def gdb_from_config():
@@ -35,6 +37,72 @@ def gdb_from_config():
 GDB = gdb_from_config()
 
 
+def get_j2rdf_builder():
+    template_folder = resolve_path("./lwua/templates")
+    log.info(f"template_folder == {template_folder}")
+    # init J2RDFSyntaxBuilder
+    j2rdf = J2RDFSyntaxBuilder(
+        templates_folder=template_folder,
+        extra_functions={"registry_of_lastmod_context": f"{URN_BASE}:ADMIN"},
+    )
+    return j2rdf
+
+
+J2RDF = get_j2rdf_builder()
+
+def get_registry_of_lastmod():
+    log.info(f"getting last modified graph")
+
+    template = "lastmod_info.sparql"
+    vars = {}
+    query = J2RDF.build_syntax(template, **vars)
+    # log.debug(f"get_admin_graph query == {query}")
+    GDB.setQuery(query)
+    GDB.setReturnFormat(JSON)
+    results = GDB.query().convert()
+
+    # convert {'head': {'vars': ['graph', 'lastmod']}, 'results': {'bindings': []}} to [{PosixPath('graph'): lastmod}]
+    # URI must be substracted from graph context and datetime str must be
+    # converted to epoch
+
+    converted = {}
+    return convert_results_registry_of_lastmod(results)
+
+def convert_results_registry_of_lastmod(results):
+    converted = {}
+    for g in results["results"]["bindings"]:
+        path = context_2_fname(g["graph"]["value"])
+        time = datetime.fromisoformat(g["lastmod"]["value"])
+        converted[path] = time
+    return converted
+
+def context_2_fname(context: str):
+    """
+    Convert a context to a filename path.
+
+    :param context: The context to convert.
+    :type context: str
+    :return: The filename corresponding to the context.
+    :rtype: str
+    """
+    assert context.startswith(
+        URN_BASE), f"Context {context} is not IRI compliant"
+    return unquote(context[len(URN_BASE) + 1:])
+
+
+def fname_2_context(fname: str):
+    """
+    Convert a filename to a context.
+
+    :param fname: The filename to convert.
+    :type fname: str
+    :return: The context corresponding to the filename.
+    :rtype: str
+    """
+    fname = str(fname)
+    return f"{URN_BASE}:{quote(fname)}"
+
+
 def uri_list(query):
     """
     Return a list of URI's from a query
@@ -42,18 +110,16 @@ def uri_list(query):
     log.debug(f"uri_list: {query}")
 
     # Extract the variable from the SELECT clause
-    select_part = re.search("SELECT(.*)WHERE", query, re.IGNORECASE).group(1)
+    select_part = re.search('SELECT(.*)WHERE', query, re.IGNORECASE).group(1)
     variables = select_part.split()
 
-    # Check that there is exactly one variable in the SELECT part of the
-    # SPARQL query
+    # Check that there is exactly one variable in the SELECT part of the SPARQL query
     if len(variables) != 1:
-        log.error(
-            "There should be exactly one variable in the SELECT part of the SPARQL query"
-        )
-        raise AssertionError(
-            "There should be exactly one variable in the SELECT part of the SPARQL query"
-        )
+        error_message = f"There should be exactly one variable in the SELECT part of the SPARQL query but found {len(variables)} in {variables}"
+        log.error(error_message)
+        raise ValueError(error_message)
+
+    var = variables[0][1:] # remove the ? from the variable
 
     GDB.setQuery(query)
     GDB.setReturnFormat(JSON)
