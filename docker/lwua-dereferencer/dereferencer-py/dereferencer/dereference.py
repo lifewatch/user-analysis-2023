2e482dcbec245c202af2d439d80ab3fcf7c56e6c
cedricdcc <30471340+cedricdcc@users.noreply.github.com>
2024-02-12 14:21:30 +0100

    Removing non used files

--- docker/lwua-dereferencer/dereferencer-py/dereferencer/dereference.py
+++ docker/lwua-dereferencer/dereferencer-py/dereferencer/dereference.py
@@ -1,39 +1,20 @@
 import logging
 import os
-import yaml
-from rdflib import Graph
-from datetime import datetime, timedelta
-from pathlib import Path
-from .helpers import resolve_path
-from .graphdb import uri_list, get_registry_of_lastmod, writeStoreToGraphDB
-from .derefEntity import DerefUriEntity
 from pytravharv import TargetStore, TravHarvConfigBuilder, TravHarvExecuter
+from .helpers import resolve_path
+from pathlib import Path
 import time
 
 log = logging.getLogger(__name__)
 
-GDB_URL = "https://localhost:7200/repositories/lwua23"
+GDB_URL = "http://graphdb:7200/repositories/lwua23"
 
 
 def config_path_from_config():
-    local_default = str(resolve_path("./configs", versus="dotenv"))
+    local_default = str(resolve_path("./config", versus="dotenv"))
     folder_name = os.getenv("CONFIG_FILES_FOLDER", local_default)
     test = "/config"
     return Path(test).absolute()
-
-
-def isExpired(lastmod: datetime, cache_lifetime: int):
-    # check if the cache_lifetime is set
-    if cache_lifetime == 0:
-        return True
-    # check if the lastmod is set
-    if lastmod is None:
-        return True
-    # check if the lastmod is older than the cache_lifetime
-    if lastmod + timedelta(minutes=cache_lifetime) < datetime.now():
-        return True
-    # if the lastmod is not older than the cache_lifetime, return false
-    return False
 
 
 class Dereference:
@@ -45,6 +26,7 @@ class Dereference:
             return TargetStore.TargetStore(url)
         except Exception as e:
             log.error("init targetstore failed, trying again in 1s")
+            log.error("error: {e}")
             time.sleep(1)
             self.init_targetstore(url)
 
@@ -52,11 +34,25 @@ class Dereference:
         log.info("running dereference")
 
         config_folder_path = config_path_from_config()
-        log.info(f"run_dereference on config files in {config_folder_path}")
+        log.info(f"run_dereference on config files in {str(config_folder_path)}")
 
-        TARGETSTORE = self.init_targetstore(GDB_URL)
+        config_files = [f for f in config_folder_path.glob("*.yml")]
+        log.info(f"config files found: {config_files}")
+
+        for f in config_files:
+            with open(f, "r") as file:
+                content = file.read()
+
+            # Log the content of the file
+            logging.info(content)
+
+        TARGETSTORE = TargetStore.TargetStore(GDB_URL)
+
+        log.info(TARGETSTORE)
+
         CONFIGBUILDER = TravHarvConfigBuilder.TravHarvConfigBuilder(
-            TARGETSTORE, str(config_folder_path)
+            TARGETSTORE,
+            str(config_folder_path),
         )
 
         CONFIGLIST = CONFIGBUILDER.build_from_folder()
@@ -64,99 +60,11 @@ class Dereference:
         for travHarvConfig in CONFIGLIST:
             log.info("Config object: {}".format(travHarvConfig()))
             prefix_set = travHarvConfig.PrefixSet
-            config_name = self.travHarvConfig.ConfigName
-            tasks = self.travHarvConfig.tasks
+            config_name = travHarvConfig.ConfigName
+            tasks = travHarvConfig.tasks
 
-            travharvexecutor = TravHarvExecutor(
+            travharvexecutor = TravHarvExecuter.TravHarvExecutor(
                 config_name, prefix_set, tasks, TARGETSTORE
             )
 
             travharvexecutor.assert_all_paths()
-
-        """
-        # get all the config files in the config folder
-        # the files should be in yml or yaml format and should start with
-        # dereference
-        config_files = [f for f in config_folder_path.glob("dereference*.yml")]
-        log.info(f"config files found: {config_files}")
-
-        # get all tasks from the graphdb
-        tasks_run = get_registry_of_lastmod()
-
-        # for each config file , parse the file and get the config
-        for config_file in config_files:
-            log.info(f"running dereference for {config_file}")
-            derefTask = DerefTask(config_file)
-
-            # log the tasks that have been run
-            log.info(f"tasks_run: {tasks_run}")
-
-            if derefTask.file_name not in tasks_run:
-                log.info(f"task {derefTask.file_name} not in tasks_run")
-                derefTask.run_deref_task()
-                continue
-
-            # check if the task is expired
-            if isExpired(tasks_run[derefTask.file_name], derefTask.cache_lifetime):
-                derefTask.run_deref_task()
-                continue
-
-            log.info(
-                f"task {derefTask.file_name} was present in GraphDB and is not expired"
-            )
-        """
-
-
-class DerefTask:
-    def __init__(self, config_file: Path):
-        self.store = Graph()
-        self.load_config(config_file)
-
-    def load_config(self, config_file):
-        with open(config_file, "r") as stream:
-            try:
-                config = yaml.safe_load(stream)
-                self.subjects = config["subjects"]
-                # check if child of subjects is SPARQL or literal
-                # if SPARQL then get the uri's from the SPARQL query
-                # if literal then get the uri's from the literal
-                if "SPARQL" in self.subjects:
-                    self.uris = uri_list(self.subjects["SPARQL"])
-                elif "literal" in self.subjects:
-                    self.uris = self.subjects["literal"]
-                else:
-                    log.error("subjects should contain either SPARQL or literal")
-                    raise Exception("subjects should contain either SPARQL or literal")
-                self.prefixes = {}
-                if "prefixes" in config:
-                    self.prefixes = config["prefixes"]
-
-                self.deref_paths = config["assert-paths"]
-                log.info(f"deref_paths: {self.deref_paths}")
-                self.cache_lifetime = (
-                    config["cache_lifetime"] if "cache_lifetime" in config else 0
-                )
-                self.file_name = config_file.name
-            except yaml.YAMLError as exc:
-                log.error(exc)
-
-    def write_store(self, filename):
-        """
-        Write the store to the graph database
-        """
-        log.info("writing store to graph database")
-        writeStoreToGraphDB(self.store, filename)
-
-    def run_deref_task(self):
-        self.store = Graph()
-        for uri in self.uris:
-            derefEntity = DerefUriEntity(
-                uri, self.deref_paths, self.store, self.prefixes
-            )
-            log.info(f"derefEntity: {derefEntity}")
-            # indent the following line to ingest after each uri is done
-            # this is better for having intermediate results in dev testing but significantly slower
-            # derefEntity.write_store(self.file_name)
-            self.store = derefEntity.store
-            # write store
-            self.write_store(self.file_name)
